79ad123a0e2ce1160bf07e253f802096b83bc574
Aksharaa Ramesh <aksharaaramesh.computing@gmail.com>
2025-08-16 14:27:33 +0800

    Initial Commit

---
+++ bodypose_tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bodypose_tracker
+//
+//  Created by T Krobot on 16/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bodypose_tracker/bodypose_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bodypose_trackerApp.swift
+//  bodypose_tracker
+//
+//  Created by T Krobot on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bodypose_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
